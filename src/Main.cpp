fe6a49fd74b3940c2314f155bc9cf8c05f14e2b6
tomeczeklmaodev <tomeczeklmao@proton.me>
2022-11-05 21:59:17 +0100

    Add new strings, add .gitignore

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,19 +9,37 @@ int main(int argc, char const *argv[])
 	// Basic things like CMD title
 	SetConsoleTitleA(("Toaster Script Loader " + SL_STR::VERSION + "; Based on " + SL_STR::CORE).c_str()); // Converting to LPCSTR from std::string, because windows.h doesn't like std::string
 
-	// The next 3 lines are used only to tell the size of the console window. Will be removed later.
-	std::cout << "Width: " << pageWidth << std::endl;
-	GetConsoleDimensionsWIN32();
-	std::cout << std::endl << std::endl;
-
 	// Header text, static, only version changes.
+	std::cout << std::endl;
 	PrintText("Toaster Script Loader " + SL_STR::VERSION + " (" + SL_STR::CORE + " " + SL_STR::CORE_VERSION + ")", Alignment::Center); std::cout << std::endl;
 	PrintText("Repository: " + SL_STR::LOADER_REPO, Alignment::Center); std::cout << std::endl;
-	PrintText("Script source: " + SL_STR::SCRIPT_SRC, Alignment::Center); std::cout << std::endl;
+	//PrintText("Script source: " + SL_STR::SCRIPT_SRC, Alignment::Center); std::cout << std::endl;
+
+	if (SL_STR::IS_PREALPHA == true)
+	{
+		PrintText(SL_STR::PREALPHA_VERSION, Alignment::Center); std::cout << std::endl;
+	}
+	else if (SL_STR::IS_ALPHA == true)
+	{
+		PrintText(SL_STR::ALPHA_VERSION, Alignment::Center); std::cout << std::endl;
+	}
+	else if (SL_STR::IS_BETA == true)
+	{
+		PrintText(SL_STR::BETA_VERSION, Alignment::Center); std::cout << std::endl;
+	}
+	else if (SL_STR::IS_RELEASECANDIDATE == true)
+	{
+		PrintText(SL_STR::RELEASECANDIDATE_VERSION, Alignment::Center); std::cout << std::endl;
+	}
+	else if (SL_STR::IS_RTM == true)
+	{
+		PrintText(SL_STR::RTM_VERSION, Alignment::Center); std::cout << std::endl;
+	}
 
 	// Testing colors (Colormod.h): colors might get useful to indicate errors and warnings
 	// Colors don't work on default Windows CMD host, only in Windows Terminal
 	// Default CMD Host displays colors as: ←[XXm; where XX is a number corresponding to color code
+	// (probably will remove color support for CMD)
 	std::cout << std::endl;
 	std::cout << Color::FG_DEFAULT << "::"; std::cout << Color::FG_BLACK << "::"; std::cout << Color::FG_RED << "::";
 	std::cout << Color::FG_GREEN << "::"; std::cout << Color::FG_YELLOW << "::"; std::cout << Color::FG_BLUE << "::";
@@ -29,12 +47,26 @@ int main(int argc, char const *argv[])
 	std::cout << Color::FG_DARK_GRAY << "::"; std::cout << Color::FG_LIGHT_RED << "::";	std::cout << Color::FG_LIGHT_GREEN << "::";
 	std::cout << Color::FG_LIGHT_YELLOW << "::"; std::cout << Color::FG_LIGHT_BLUE << "::";	std::cout << Color::FG_LIGHT_MAGENTA << "::";
 	std::cout << Color::FG_LIGHT_CYAN << "::"; std::cout << Color::FG_WHITE << "::"; std::cout << Color::FG_DEFAULT;
+	std::cout << std::endl << std::endl;
+
+	std::cout << Color::FG_RED << "Critical Error" << std::endl;
+	std::cout << Color::FG_LIGHT_RED << "Error" << std::endl;
+	std::cout << Color::FG_YELLOW << "Warning" << std::endl;
+	std::cout << Color::FG_GREEN << "Success" << std::endl;
+	std::cout << Color::FG_DEFAULT;
+
+	std::cout << std::endl << std::endl;
+
+	// The next 3 lines are used only to tell the size of the console window. Will be removed later.
+	std::cout << "Width: " << pageWidth << std::endl;
+	GetConsoleDimensionsWIN32();
 	std::cout << std::endl;
 
-	// Preparing for arguments
+	// Arguments setup; will be moved to a separate file
+	// Display all arguments received
 	for (int i = 0; i < argc; i++)
 	{
-		std::cout << "[recv-arg@pos:" << i << "]: " << argv[i] << "\n"; // printing argument recieved and its position
+		std::cout << "[recv-arg@pos:" << i << "]: " << argv[i] << "\n";
 	}
 
 	getchar();
--- src/headers/SL_Strings.h
+++ src/headers/SL_Strings.h
@@ -19,10 +19,21 @@
 //============================================================
 namespace SL_STR
 {
-	std::string CORE         = "Valkyrie"; // Core/Loader name
-	std::string CORE_VERSION = "0.1-dev";
-	std::string VERSION      = "0.1-dev";
-	std::string LOADER_REPO  = "https://github.com/tomeczeklmaodev/toasterscriptloader/";
-	std::string SCRIPT_SRC   = "tomeczeklmaodev/scripts/"; // This repository does not exist, will be created later
-	std::string SCRIPT_EXT   = "vslx"; // File extension for scripts
+	std::string CORE                     = "Valkyrie"; // Core/Loader name
+	std::string CORE_LOWER               = "valkyrie"; // Core/Loader name (lowercase)
+	std::string CORE_VERSION             = "0.1-dev";
+	std::string VERSION                  = "0.1-dev";
+	std::string LOADER_REPO              = "https://github.com/tomeczeklmaodev/toasterscriptloader/";
+	std::string SCRIPT_SRC               = "tomeczeklmaodev/scripts/"; // This repository does not exist, will be created later
+	std::string SCRIPT_EXT               = "vslx"; // File extension for scripts
+	bool IS_PREALPHA                     = true;
+	bool IS_ALPHA                        = false;
+	bool IS_BETA                         = false;
+	bool IS_RELEASECANDIDATE             = false;
+	bool IS_RTM                          = false;
+	std::string PREALPHA_VERSION         = CORE_LOWER + "_" + CORE_VERSION + ":(pre-alpha build)";
+	std::string ALPHA_VERSION            = CORE_LOWER + "_" + CORE_VERSION + ":(alpha build)";
+	std::string BETA_VERSION             = CORE_LOWER + "_" + CORE_VERSION + ":(beta build)";
+	std::string RELEASECANDIDATE_VERSION = CORE_LOWER + "_" + CORE_VERSION + ":(release-candidate build)";
+	std::string RTM_VERSION              = CORE_LOWER + "_" + CORE_VERSION + ":(rtm build)";
 }
